81ff7699e59ef0a9c1857881794ad563fd078676
Valentin Yuryev <goodfaiter@gmail.com>
2025-12-18 16:00:40 +0000

    updated setups

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-# pip3 install /workspace/tools/vlearn/vlearn-0.2.7-cp310-cp310-linux_x86_64.whl
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}/workspace/.venv/lib/python3.10/site-packages/vlearn/lib"
-export CUDA_VISIBLE_DEVICES=0
-export __NV_PRIME_RENDER_OFFLOAD=1
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
 
 # Install the package in editable mode
 # cd /workspace
-# uv pip install -e workspace/
+# uv pip install -e .
 
 # Run the command passed to the container
 exec "$@"
--- setenv.sh
+++ setenv.sh
@@ -3,3 +3,6 @@ export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}/workspace/.venv/li
 export CUDA_VISIBLE_DEVICES=0
 export __NV_PRIME_RENDER_OFFLOAD=1
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export WANDB_API_KEY=eec35c2a3ec81aeed2deaeb37585e1d2ab561f5f
+export VL_TURBO_ACTIVATE_PATH=/workspace/tools/vlearn/TurboActivate.dat
+export VL_LICENSE_KEY_PATH=/vsim_tech/License.key
