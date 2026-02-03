4efcd7012d1f7ea9f7f6176ecb27d192b1ff2bff
Valentin Yuryev <goodfaiter@gmail.com>
2026-02-03 14:20:16 +0100

    better entry point script that installs uv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Install the package in editable mode
-# cd /workspace
-# uv pip install -e .
+cd workspace
+uv sync
+uv pip install -e .
 
 # Run the command passed to the container
 exec "$@"
