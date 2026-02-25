1e358d42602238e40d8a794dc1cec40849988d01
Valentin Yuryev <goodfaiter@gmail.com>
2026-02-25 16:21:05 +0000

    reverted buffer

--- src/optimal_morphology_rl/tendon_model/buffer.py
+++ src/optimal_morphology_rl/tendon_model/buffer.py
@@ -1,20 +1,13 @@
 import torch
 
 class TimeSeriesBuffer:
-    def __init__(self, num_envs, max_size, feature_dim=None, device='cpu'):
-        self.feature_dim = feature_dim
+    def __init__(self, num_envs, max_size, device='cpu'):
+        self.buffer = torch.zeros((num_envs, max_size), device=device)
         self.max_size = max_size
-        if feature_dim is None:
-            self.buffer = torch.zeros((num_envs, max_size), device=device)
-        else:
-            self.buffer = torch.zeros((num_envs, max_size, feature_dim), device=device)
-
-    def add(self, value: torch.Tensor):
+    
+    def add(self, value):
         self.buffer = torch.roll(self.buffer, -1, dims=1)
-        if self.feature_dim is None:
-            self.buffer[:, -1] = value.squeeze(-1)
-        else:
-            self.buffer[:, -1, :] = value
+        self.buffer[:, -1] = value.squeeze()
 
     def reset(self, reset_mask: torch.Tensor):
-        self.buffer[reset_mask] = 0.0
+        self.buffer[reset_mask, :] = 0.0
